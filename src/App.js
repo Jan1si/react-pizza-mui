5a05c7dae8dd5c9bcfe4c262134828a04b239a6c
Nikita <nikita.evgrafov@inbox.ru>
2022-10-13 19:35:56 +0500

    complite comonent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Container } from '@mui/system';
 import {Header} from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Container maxWidth="lg" sx={{
+        backgroundColor: 'white',
+        margin: '50px auto',
+        borderRadius: '10px',
+      }}>
+        <Header />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /> 
+      </Container>
+      
+
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,60 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import React from 'react'
 import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
-    <div>index</div>
+    <Box sx={{
+      padding: '0px 10px',
+    }}>
+      <AppBar position={"static"} sx={{
+        boxShadow: 'none',
+        borderBottom: '1px solid #F6F6F6',
+        backgroundColor: 'white',
+      }}>
+        <Toolbar className={styles.navbar} >
+          <Box sx={{
+            display: "flex",
+            alignItems: 'center',
+          }}>
+            <img src="./img/logo-pizza.svg" alt="" />
+            <Box sx={{
+              display: 'flex',
+              flexDirection: "column",
+              ml: '17px'
+            }}>
+              <Typography variant={'h5'} component={'h1'} sx={{
+                color: 'black',
+                fontFamily: 'Proxima Nova-eb',
+                fontWeight: '800',
+                textTransform: 'uppercase',
+                lineHeight: '20px'
+
+              }}>
+                React Pizza
+              </Typography>
+              <Typography sx={{
+                color: '#7B7B7B',
+                fontSize: '16px',
+                fontFamily: 'Proxima Nova',
+                fontWeight: '400',
+                lineHeight: '20px'
+              }}>
+                самая вкусная пицца во вселенной
+              </Typography>
+            </Box>
+          </Box>
+         <Button variante="contained" className={styles.custom__btn}>
+              <div className={styles.total__price}>
+                <p>520 .р</p>
+              </div>
+              <div className={styles.len__product}>
+                <img src="./img/cart.svg" alt="" />
+                <p>3</p>
+              </div>
+         </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
